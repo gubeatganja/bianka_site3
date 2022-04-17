4fd00e2c3fcee8511390ccb08fea84232253ac50
Vladyslav Chumak <59369387+gubeatganja@users.noreply.github.com>
2022-04-17 17:17:42 +0200

    Update settings.py

--- Bianka/settings.py
+++ Bianka/settings.py
@@ -136,6 +136,8 @@ LOCALE_PATHS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'Bianka', 'staticfiles')
+
 STATIC_URL = 'static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
