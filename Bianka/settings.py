b4155611fee3407d6d5da829befec21c85f7fc57
Vladyslav <gubeatganja@gmail.com>
2022-04-25 20:35:22 +0200

    25_04_new_settings_for_heroku

--- Bianka/settings.py
+++ Bianka/settings.py
@@ -137,7 +137,7 @@ LOCALE_PATHS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'bianka_site')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = []
