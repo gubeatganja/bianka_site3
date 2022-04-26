3129bbcdc863df21b1cb7da8d1fd826ea284371c
Vladyslav <gubeatganja@gmail.com>
2022-04-26 10:18:18 +0200

    26_04_2

--- Bianka/settings.py
+++ Bianka/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^0l4_*e5(2w0*(3lq)l&-wx9604j*1uika*r-e2$ta3_hnu7l7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['bianka-blog-2.herokuapp.com', '127.0.0.1']
 
